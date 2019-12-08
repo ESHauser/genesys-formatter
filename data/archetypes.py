bd58b0a939db1479f20384f544ea0621bd016e4d
Dudley Fox <dudley.fox@gmail.com>
2019-12-08 15:33:55 -0600

    Reorganize the data

---
+++ data/archetypes.py
@@ -0,0 +1,72 @@
+archetypes = {
+	"AverageHuman" : {
+		"StartingStats" : {
+			"Brawn" : 2,
+			"Agility" : 2,
+			"Intellect" : 2,
+			"Cunning" : 2,
+			"Willpower" : 2,
+			"Presence" : 2
+		},
+		"StartingTalents" : {
+			"Ready for Anything"
+		},
+		"WoundThreshold" : 10,
+		"StrainThreshold" : 10
+	},
+	"Laborer" : {
+		"StartingStats" : {
+			"Brawn" : 3,
+			"Agility" : 2,
+			"Intellect" : 2,
+			"Cunning" : 2,
+			"Willpower" : 1,
+			"Presence" : 2	
+		},
+		"StartingSkills" : {
+			"Athletics" : 1
+		},
+		"StartingTalents" : {
+			"Tough as Nails"
+		}
+		,
+		"WoundThreshold" : 12,
+		"StrainThreshold" : 8
+	},
+	"Intellectual" : {
+		"StartingStats" : {
+			"Brawn" : 2,
+			"Agility" : 1,
+			"Intellect" : 3,
+			"Cunning" : 2,
+			"Willpower" : 2,
+			"Presence" : 2	
+		},
+		"StartingSkills" : {
+			"Knowledge" : 1
+		},
+		"StartingTalents" : {
+			"Brilliant!"
+		},
+		"WoundThreshold" : 8,
+		"StrainThreshold" : 12
+	},
+	"Aristocrat" : {
+		"StartingStats" : {
+			"Brawn" : 1,
+			"Agility" : 2,
+			"Intellect" : 2,
+			"Cunning" : 2,
+			"Willpower" : 2,
+			"Presence" : 3
+		},
+		"StartingSkills" : {
+			"Cool" : 1
+		},
+		"StartingTalents" : {
+			"Forceful Personality"
+		},
+		"WoundThreshold" : 10,
+		"StrainThreshold" : 10
+	}
+}
---
+++ data/armor.py
@@ -0,0 +1,30 @@
+armor = {
+	"riotArmor" : {
+		"name" : "Riot Armor",
+		"soak" : 2,
+		"meleeDefense" : 0,
+		"rangedDefense" : 0,
+		"encum" : 3
+	}, 
+	"heavyJacket" : {
+		"name" : "Heavy Jacket",
+		"soak" : 1,
+		"meleeDefense" : 0,
+		"rangedDefense" : 0,
+		"encum" : 1
+	}, 
+	"lightBodyArmor" : {
+		"name" : "Light Body Armor",
+		"soak" : 2,
+		"meleeDefense" : 0,
+		"rangedDefense" : 0,
+		"encum" : 2
+	}, 
+	"durableClothing" : {
+		"name" : "Durable Clothing",
+		"soak" : 1,
+		"meleeDefense" : 0,
+		"rangedDefense" : 0,
+		"encum" : 1
+	}
+}
